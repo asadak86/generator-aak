5f45878f01585fbe62ac0d5ad06c13c67e7ce920
Asad Ali Kanwal <gotoasad@gmail.com>
2015-02-11 02:10:36 +0400

    Update index.js

--- section/index.js
+++ section/index.js
@@ -4,14 +4,14 @@ var yeoman = require('yeoman-generator');
 
 var SectionGenerator = yeoman.generators.NamedBase.extend({
   generateSection: function(){
-    // var context = {
-    //     content: this.name,
-    //     id: this._.classify(this.name)
-    // }
+    var context = {
+        content: this.name,
+        id: this._.classify(this.name)
+    }
 
-    // var fileBase = Date.now() + "_" + this._.underscored(this.name);
-    // var htmlFile = "app/sections/" + fileBase + ".html";
-    // var cssFile  = "app/css/" + fileBase + ".css";
+    var fileBase = Date.now() + "_" + this._.underscored(this.name);
+    var htmlFile = "app/sections/" + fileBase + ".html";
+    var cssFile  = "app/css/" + fileBase + ".css";
 
     //this.template("_section.html", htmlFile, context);
     //this.template("_section.css", cssFile, context);
